c327a7bd137c157a7bacc4081f0bd0e3ade680e6
Igor Sharov <IgorSharov@users.noreply.github.com>
2019-10-04 15:54:17 +0300

    Sidebar close button

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,8 @@ export default class App extends Component<Props, State> {
   // state = { visible: true }; //TODO: clean up
   state = { visible: false };
 
-  toggleVisible = () => this.setState(state => ({ visible: !state.visible }));
+  toggleVisibleShow = () => this.setState({ visible: true });
+  toggleVisibleHide = () => this.setState({ visible: false });
 
   render() {
     const { visible } = this.state;
@@ -28,15 +29,15 @@ export default class App extends Component<Props, State> {
           icon="labeled"
           vertical
           visible={visible}
-          onHide={this.toggleVisible}
+          onHide={this.toggleVisibleHide}
           width="wide"
         >
-          {/* <Button
-            onClick={this.toggleVisible}
+          <Button
+            onClick={this.toggleVisibleHide}
             size="big"
             icon={{ name: "close", size: "large" }}
             className="menu-button-close"
-          /> */}
+          />
           <Menu.Item>
             <Image
               src="https://static.wixstatic.com/media/a4894d_bc50df6799ad42e9aba766682c827da3~mv2.png/v1/fill/w_427,h_427,al_c,q_80,usm_0.66_1.00_0.01/blank.webp"
@@ -63,7 +64,7 @@ export default class App extends Component<Props, State> {
         </Sidebar>
 
         <Sidebar.Pusher dimmed={visible}>
-          <Header clickSidebarMenu={this.toggleVisible} />
+          <Header clickSidebarMenu={this.toggleVisibleShow} />
         </Sidebar.Pusher>
       </Sidebar.Pushable>
     );

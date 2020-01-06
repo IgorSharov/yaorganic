594e30aa29985a9cd61ada6fd265574fb59f9b44
Igor Sharov <IgorSharov@users.noreply.github.com>
2020-01-06 18:42:42 +0300

    Formik base form

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,8 +1,60 @@
 import React from "react";
 import "./Home.scss";
 
-interface Props {}
+import { Formik, Form, Field, ErrorMessage, FormikErrors } from "formik";
+import { Container, Button, Header } from "semantic-ui-react";
 
-const Header: React.FC<Props> = () => null;
+interface FormValues {
+  country: string;
+  description: string;
+  name: string;
+}
 
-export default Header;
+const Home: React.FC<{}> = () => {
+  const initialValues: FormValues = { country: "", description: "", name: "" };
+
+  return (
+    <Container>
+      <Header as="h3">Форма Вендора</Header>
+      <Formik
+        initialValues={initialValues}
+        validate={(values: FormValues) => {
+          const errors: FormikErrors<FormValues> = {};
+          if (!values.country) {
+            errors.country = "Required";
+          }
+          if (!values.description) {
+            errors.description = "Required";
+          }
+          if (!values.name) {
+            errors.name = "Required";
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting, resetForm }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+            resetForm();
+          }, 400);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <Field type="text" name="country" />
+            <ErrorMessage name="country" component="div" className="class1" />
+            <Field type="text" name="description" />
+            <ErrorMessage name="description" component="div" />
+            <Field type="text" name="name" />
+            <ErrorMessage name="name" component="div" />
+            <Button type="submit" className="primary" disabled={isSubmitting}>
+              Submit
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Container>
+  );
+};
+
+export default Home;

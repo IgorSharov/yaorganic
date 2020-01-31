2f72368beec73d254e1126e4d954ca69c9202125
Igor Sharov <IgorSharov@users.noreply.github.com>
2020-01-31 13:36:44 +0300

    Updates

---
+++ precache-manifest.5b64d5bf5793e773a6ceea108dda1de1.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aff843c80517db80d16e82369d2f3699",
+    "url": "/yaorganic/index.html"
+  },
+  {
+    "revision": "0241db1cd667d7c3e676",
+    "url": "/yaorganic/static/css/2.5e304c62.chunk.css"
+  },
+  {
+    "revision": "e62115cf5bcd10e0d344",
+    "url": "/yaorganic/static/css/main.3a5ade57.chunk.css"
+  },
+  {
+    "revision": "0241db1cd667d7c3e676",
+    "url": "/yaorganic/static/js/2.930e9977.chunk.js"
+  },
+  {
+    "revision": "53e212d0cdcf90b86edf63f5b94320d5",
+    "url": "/yaorganic/static/js/2.930e9977.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e62115cf5bcd10e0d344",
+    "url": "/yaorganic/static/js/main.0b9c4612.chunk.js"
+  },
+  {
+    "revision": "63ef4cc37216153116f0",
+    "url": "/yaorganic/static/js/runtime-main.da2fb769.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/yaorganic/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/yaorganic/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/yaorganic/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/yaorganic/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/yaorganic/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/yaorganic/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/yaorganic/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/yaorganic/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/yaorganic/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/yaorganic/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/yaorganic/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/yaorganic/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/yaorganic/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/yaorganic/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/yaorganic/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/yaorganic/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
